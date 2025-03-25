cb70b2c6140041a3a1c3c8ad954d6f7afb773ea9
Nate Eagle <nate@nateeagle.com>
2025-03-25 16:54:25 -0400

    Improve mobile typography for names on about us

--- src/app/about-us/Person.tsx
+++ src/app/about-us/Person.tsx
@@ -22,7 +22,7 @@ const Person = ({ name, title, children }: Props) => {
         "[&_p:last-of-type]:mb-0",
       ])}
     >
-      <h2 className="text-xl">{name}</h2>
+      <h2 className="text-xl leading-tight mb-1 md:mb-0">{name}</h2>
       <h3 className="text-accent! mb-4 font-normal! normal-case! font-sans! leading-tight">
         {title}
       </h3>
