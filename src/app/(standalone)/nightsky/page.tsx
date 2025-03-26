deae77694d305331e083c4280d95b5572fec598d
Nate Eagle <nate@nateeagle.com>
2025-03-26 17:13:26 -0400

    Add themed favicon

--- src/app/(standalone)/nightsky/page.tsx
+++ src/app/(standalone)/nightsky/page.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next";
 import React from "react";
 import Nightsky from "@/app/components/Nightsky";
 import FullscreenWrapper from "@/app/components/FullscreenWrapper";
+
+export const metadata: Metadata = {
+  title: "Starry Night Sky",
+  description:
+    "A full page of a starry night sky. Change the number of stars by changing the `stars` query param.",
+};
 
 const Page = () => {
   return (
