84c4efe72790178e4d7e7e2851b69248853f9919
Nate Eagle <nate@nateeagle.com>
2025-03-26 17:14:09 -0400

    only get upcoming shows by default

--- src/app/actions/shows.tsx
+++ src/app/actions/shows.tsx
@@ -1,31 +1,46 @@
 import type { Show } from "@/app/types";
 import wixClient from "@/lib/wixClient";
 
-export const getShowsWithData = async ({ shows }: { shows: Show[] }) => {
+type Props = {
+  shows: Show[];
+  onlyUpcoming?: boolean;
+};
+
+export const getShowsWithData = async ({
+  shows,
+  onlyUpcoming = true,
+}: Props) => {
   "use server";
 
   const showsWithData = await Promise.all(
     shows.map(async (show: Show) => {
+      const castQuery = wixClient.items
+        .query("Credits")
+        .eq("show", show._id)
+        .eq("category", "cast")
+        .include("person")
+        .ascending("order");
+
+      const crewQuery = wixClient.items
+        .query("Credits")
+        .eq("show", show._id)
+        .eq("category", "crew")
+        .include("person")
+        .ascending("order");
+
+      let showsQuery = wixClient.wixEventsV2
+        .queryEvents()
+        .eq("title", show.title)
+        .ascending("dateAndTimeSettings.startDate");
+
+      if (onlyUpcoming) {
+        showsQuery = showsQuery.eq("status", "UPCOMING");
+      }
+
       const [cast, crew, events] = await Promise.all([
-        wixClient.items
-          .query("Credits")
-          .eq("show", show._id)
-          .eq("category", "cast")
-          .include("person")
-          .ascending("order")
-          .find(),
-        wixClient.items
-          .query("Credits")
-          .eq("show", show._id)
-          .eq("category", "crew")
-          .include("person")
-          .ascending("order")
-          .find(),
-        wixClient.wixEventsV2
-          .queryEvents()
-          .eq("title", show.title)
-          .ascending("dateAndTimeSettings.startDate")
-          .find(),
+        castQuery.find(),
+        crewQuery.find(),
+        showsQuery.find(),
       ]);
 
       return {
