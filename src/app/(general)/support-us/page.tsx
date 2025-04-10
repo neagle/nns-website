0ac0c0fb54638b57408ca2643e49baf69bc918c8
Nate Eagle <nate@nateeagle.com>
2025-04-09 21:31:28 -0400

    Update support us page

--- src/app/(general)/support-us/page.tsx
+++ src/app/(general)/support-us/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import React from "react";
 import Link from "next/link";
-import HeroSection from "@/app/components/HeroSection";
+import Image from "next/image";
 import classnames from "classnames";
 
 export const metadata: Metadata = {
@@ -11,56 +11,73 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <div>
-      <HeroSection
-        imageUrl="/images/photographs/general/all-the-board.jpg"
-        className={classnames(["h-[40vh]", "xl:h-[60vh]"])}
-      >
-        <h1>Support NOVA Nightsky Theater with a donation!</h1>
-      </HeroSection>
-      <div className="prose">
-        <p>
-          We are an unconventional community theater company powered by
-          unconventional people in our community.
-        </p>
+    <div className="p-4 md:p-6 xl:p-8 ">
+      <h1 className="text-2xl mb-4">
+        Support NOVA Nightsky Theater with a donation
+      </h1>
+      <div className={classnames(["md:grid", "md:grid-cols-2", "gap-8"])}>
+        <div className="prose mb-8 md:mb-0">
+          <p>
+            We are an unconventional community theater company powered by
+            unconventional people in our community.
+          </p>
 
-        <p>
-          We believe in the power of expression and our volunteers work day and
-          night to make sure our performances bring our audience members joy. We
-          believe that the most important thing about community theater is the
-          community of people who create it. A donation to NOVA Nightsky allows
-          us to:
-        </p>
+          <p>
+            We believe in the power of expression and our volunteers work day
+            and night to make sure our performances bring our audience members
+            joy. We believe that the most important thing about community
+            theater is the community of people who create it. A donation to NOVA
+            Nightsky allows us to:
+          </p>
 
-        <ul>
-          <li>
-            pay for rehearsal and performance space that gives us flexibility to
-            build rehearsal schedules that work for our cast and crew
-          </li>
+          <ul>
+            <li>
+              Pay for rehearsal and performance space that gives us flexibility
+              to build rehearsal schedules that work for our cast and crew
+            </li>
 
-          <li>compensate playwrights and other artists for their work</li>
+            <li>Compensate playwrights and other artists for their work</li>
 
-          <li>apply for rights from publishers to perform shows</li>
+            <li>Apply for rights from publishers to perform shows</li>
 
-          <li>rent/purchase technical equipment</li>
+            <li>Rent/purchase technical equipment</li>
 
-          <li>
-            cover costs associated with each individual show including costumes,
-            props, set pieces, and construction
-          </li>
-        </ul>
-      </div>
+            <li>
+              Cover costs associated with each individual show including
+              costumes, props, set pieces, and construction
+            </li>
+          </ul>
+        </div>
 
-      <div className="flex flex-col gap-4 md:flex-row">
-        <Link
-          className="btn btn-primary"
-          href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=novanightskytheater@gmail.com&item_name=NOVA%20Nightsky%20Theater&currency_code=USD"
-        >
-          Give Today
-        </Link>
-        <Link className="btn" href="/box-office">
-          Become a Subscriber
-        </Link>
+        <div className={classnames([])}>
+          <Image
+            className="w-full rounded-lg shadow-md mb-4 shadow-primary"
+            src="/images/photographs/general/all-the-board.jpg"
+            width="1336"
+            height="726"
+            alt="NOVA Nightsky's Board: Adam Ressa, Jaclyn Robertson, and Sarah Baczewski"
+          />
+          <div className={classnames(["flex", "gap-4"])}>
+            <Link
+              className={classnames([
+                "btn",
+                "btn-primary",
+                "grow",
+                "hover:scale-110",
+                "transition-all",
+              ])}
+              href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=novanightskytheater@gmail.com&item_name=NOVA%20Nightsky%20Theater&currency_code=USD"
+            >
+              Give Today
+            </Link>
+            <Link
+              className={classnames(["btn", "grow"])}
+              href="/box-office/subscriptions"
+            >
+              Become a Subscriber
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
