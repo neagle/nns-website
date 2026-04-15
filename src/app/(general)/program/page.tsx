27c7ff3a5ca3e4103f9cf2e36d44c001e8dd3e5f
Nate Eagle <neagle@leolabs.space>
2026-04-15 15:45:35 -0400

    Change criterion for displaying program

--- src/app/(general)/program/page.tsx
+++ src/app/(general)/program/page.tsx
@@ -15,8 +15,6 @@ export const metadata: Metadata = {
 const getShows = async () => {
   const { items } = await wixClient.items
     .query("Shows")
-    // Only get shows that have opened in the past (or today)
-    .le("openingDate", new Date().toISOString())
     // Only get shows that have a program
     .ne("program", null)
     .descending("openingDate")
@@ -35,7 +33,7 @@ const Program = async () => {
         key={shows[0]._id}
         href={media.getDocumentUrl(shows[0].program!).url}
         className={classnames([
-          "flex",
+          "inline-flex",
           "ml-[-3px]",
           "items-center",
           "link",
@@ -44,19 +42,16 @@ const Program = async () => {
           "transition-all",
           "duration-250",
           "hover:[&>svg]:scale-120",
+          "w-full",
+          "md:max-w-1/2",
+          "lg:max-w-1/3",
         ])}
       >
         <ShowLogo
           show={shows[0]}
           link={false}
           // targetWidth={150}
-          className={classnames([
-            "text-center",
-            "overflow-hidden",
-            "w-full",
-            "md:max-w-1/2",
-            "lg:max-w-1/3",
-          ])}
+          className={classnames(["text-center", "overflow-hidden"])}
         />
       </Link>
 
